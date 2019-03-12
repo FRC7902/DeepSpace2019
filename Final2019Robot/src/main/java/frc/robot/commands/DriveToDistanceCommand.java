a2f281b8a84556b8b25f14c097aafd989b7ce2b6
MasterSeafood <yellowwatermelon14@gmail.com>
2019-03-12 11:53:11 -0400

    no drive to distance

--- Final2019Robot/src/main/java/frc/robot/commands/DriveToDistanceCommand.java
+++ Final2019Robot/src/main/java/frc/robot/commands/DriveToDistanceCommand.java
@@ -40,7 +40,7 @@ public class DriveToDistanceCommand extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    Robot.driveSubsystem.drive(speed, 0);
+    //Robot.driveSubsystem.drive(speed, 0);
   }
 
   // Make this return true when this Command no longer needs to run execute()
